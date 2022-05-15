0cf9f51f2db594f1809ecfd9ce045997bf8e937f
nguquen <thienlduc@gmail.com>
2022-05-15 13:01:21 +0700

    [wallet] inject variables via dockerfile args

--- contract-helper/start.sh
+++ contract-helper/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp /root/.near/localnet/node0/validator_key.json .
+cp /root/.near/localnet/validator_key.json .
 
 ACCOUNT_CREATOR_KEY=$(cat validator_key.json | tr -d " \t\n\r") && echo "ACCOUNT_CREATOR_KEY=$ACCOUNT_CREATOR_KEY" >> .env
 
