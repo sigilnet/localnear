7d5edec0237eeb845e38bdfdeb8cdbdaa9ec8965
nguquen <thienlduc@gmail.com>
2022-08-05 22:04:05 +0700

    Update wallet

--- contract-helper/start.sh
+++ contract-helper/start.sh
@@ -4,4 +4,4 @@ cp /root/.near/localnet/validator_key.json .
 
 ACCOUNT_CREATOR_KEY=$(cat validator_key.json | tr -d " \t\n\r") && echo "ACCOUNT_CREATOR_KEY=$ACCOUNT_CREATOR_KEY" >> .env
 
-yarn start
+yarn $START_TYPE
